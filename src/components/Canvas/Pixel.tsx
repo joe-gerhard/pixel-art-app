1d03467bf8af890051e5aa5267f7e5ee55945320
Joe Gerhard <jlgerhard@gmail.com>
2020-01-30 17:04:12 -0600

    Add mouseEnter handler to pixels to accommodate user clicking and holding mouse to draw

--- src/components/Canvas/Pixel.tsx
+++ src/components/Canvas/Pixel.tsx
@@ -1,7 +1,8 @@
 import React, { Dispatch } from 'react'
 import { Styled } from './styles';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { CanvasActions } from '../../redux/actions/CanvasActions';
+import { AppState } from '../../redux/reducers/rootReducer';
 
 interface IProps {
   color: string;
@@ -12,14 +13,20 @@ interface IProps {
 
 const Pixel: React.FC<IProps> = ({ color, x, y}) => {
 
+  const { mouseDown } = useSelector((state: AppState) => state.userInput);
+
   const dispatch = useDispatch<Dispatch<CanvasActions>>();
 
   const handleChangePixelColor = (): void => {
     dispatch({ type: 'COLOR_PIXEL', pixel: { x, y }, color: '#FF0000'})
   }
 
+  const handleMouseEnter = (): void => {
+    if(mouseDown) handleChangePixelColor()
+  }
+
   return (
-    <Styled.Pixel color={color} onClick={handleChangePixelColor} />
+    <Styled.Pixel color={color} onClick={handleChangePixelColor} onMouseEnter={handleMouseEnter} />
   )
 }
 
