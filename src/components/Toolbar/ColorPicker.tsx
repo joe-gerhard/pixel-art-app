b0e6a7c719fd78033f402c78bf9f74811003452d
Joe Gerhard <jlgerhard@gmail.com>
2020-01-31 23:36:52 -0600

    Add color picker visual structure

--- src/components/Toolbar/ColorPicker.tsx
+++ src/components/Toolbar/ColorPicker.tsx
@@ -3,15 +3,14 @@ import { Styled } from './styles';
 
 const ColorPicker: React.FC = () => {
 
-  const colors: string[] = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
   return (
     <Styled.ColorPicker>
-      {colors.map(color => 
-        <Styled.Pixel color={color} />
-      )}
+      
     </Styled.ColorPicker>
   )
 }
 
 export default ColorPicker;
+
+
--- src/components/Toolbar/styles.ts
+++ src/components/Toolbar/styles.ts
@@ -7,6 +7,10 @@ type IProps = {
 }
 
 const Toolbar = styled('section')<IProps>(({ theme }) => css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
   background: ${theme.primary};
   height: 600px;
   width: 150px;
@@ -14,20 +18,17 @@ const Toolbar = styled('section')<IProps>(({ theme }) => css`
 `);
 
 const ColorPicker = styled('div')<IProps>(({ theme }) => css`
-  background: ${theme.light};
+  background: linear-gradient(0deg, rgba(0,0,0, 1), rgba(0,0,0,0)),              
+              linear-gradient(90deg, rgba(255,255,255, 1), rgba(255,255,255, 0));
+  background-color: green;
+  height: 100px;
+  width: 100px;
   display: flex;
   justify-content: center;
   align-items: center;
 `);
 
-const Pixel = styled('div')<IProps>(({ theme, color }) => css`
-  height: 1px;
-  width: 1px; 
-  background: ${color};
-`);
-
 export const Styled = {
   Toolbar,
   ColorPicker,
-  Pixel,
 }
